39d76762137c1df147cea5ae64e9f9b3a8a4a816
Uday Kumar <udaykrishnaut@gmail.com>
2026-03-13 19:23:02 +0530

    Removed API keys and updated client file

--- bot/client.py
+++ bot/client.py
@@ -1,7 +1,7 @@
 from binance.client import Client
 
-API_KEY = "3CclHHvBYuIT30OBZ0p0wkDBIsHfWKnAwTyo5PYbxepO9uEH85ZdOPe7BPSnTMMM"
-API_SECRET = "rKdKeGtZg37gNZbr9ZJnfNHo4kwfm5y38baV5zdyhHDnV31yAKhNj6MtvRe1Sc6L"
+API_KEY = "here we can paste our API_Key"
+API_SECRET = "here paste the API_Secret"
 
 def get_client():
     client = Client(API_KEY, API_SECRET)
